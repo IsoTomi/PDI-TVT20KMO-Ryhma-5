35091e8ac513c8f630889c171af5d4f218168563
JimiLinn <70435082+JimiLinn@users.noreply.github.com>
2022-03-30 18:25:52 +0300

    Restart
    Restarting project

--- packages/flame/lib/src/effects/provider_interfaces.dart
+++
@@ -1,7 +0,0 @@
-import 'package:vector_math/vector_math_64.dart';
-
-/// Interface for a component that can be affected by move effects.
-abstract class PositionProvider {
-  Vector2 get position;
-  set position(Vector2 value);
-}
