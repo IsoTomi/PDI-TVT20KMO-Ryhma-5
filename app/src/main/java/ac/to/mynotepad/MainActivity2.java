efc34e8e7e2dd86419bd72fb29a8932e6e2099a9
IsoTomi <tomiisoj1984@gmail.com>
2022-04-25 18:57:31 +0300

    Authentication ready

---
+++ app/src/main/java/ac/to/mynotepad/MainActivity2.java
@@ -0,0 +1,44 @@
+package ac.to.mynotepad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import ac.to.mynotepad.LoginActivity;
+import ac.to.mynotepad.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        /*btnLogOut.setOnClickListener(view -> {
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity2.this, LoginActivity.class));
+        });*/
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity2.this, LoginActivity.class));
+        }
+    }
+}
